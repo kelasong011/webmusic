2136282aa8eaea5705f10d0e4083e3b96a47b6ea
smoker <68688900+kelasong011@users.noreply.github.com>
2021-02-08 21:16:37 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -77,10 +77,10 @@ import moment from 'moment';
 // 全局过滤器
 Vue.filter('formatDuration',(dt)=>{
   // 转分
-  let min = Math.ceil(dt / 1000 / 60);
+  let min = Math.floor(dt / 1000 / 60);
   min = min < 10 ? '0' + min : min;
   // 秒
-  let sec = Math.ceil((dt / 1000) % 60);
+  let sec = Math.floor((dt / 1000) % 60);
   sec = sec < 10 ? '0' + sec : sec;
   return min + ':' + sec;
 })
